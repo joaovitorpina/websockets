4a0f58c76f29670062bc8d980210122cb98c695a
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2021-08-24 16:29:05 -0300

    Initial commit

---
+++ mock-srv/plugins/data-utils.js
@@ -0,0 +1,65 @@
+'use strict'
+const { PassThrough } = require('stream')
+const fp = require('fastify-plugin')
+
+const orders = {
+    A1: { total: 3 },
+    A2: { total: 7 },
+    B1: { total: 101 },
+}
+
+const catToPrefix = {
+    electronics: 'A',
+    confectionery: 'B'
+}
+
+const orderStream = new PassThrough({ objectMode: true })
+
+async function * realtimeOrders() {
+    for await (const { id, total } of orderStream) {
+        yield JSON.stringify({ id, total })
+    }
+}
+
+function addOrder(id, amount) {
+    if (orders.hasOwnProperty(id) === false) {
+        const err = Error(`Order ${id} not found`)
+        err.status = 404
+        throw err
+    }
+    if (Number.isInteger(amount) === false) {
+        const err = Error('Supplied amount must be an integer')
+        err.status = 400
+        throw err
+    }
+    orders[id].total += amount
+    const { total } = orders[id]
+    orderStream.write({ id, total })
+}
+
+function * currentOrders(category) {
+    const idPrefix = catToPrefix[category]
+    if (!idPrefix) return
+    const ids = Object.keys(orders).filter((id) => id[0] === idPrefix)
+    for (const id of ids) {
+        yield JSON.stringify({id, ...orders[id]})
+    }
+}
+
+const calculateID = (idprefix, data) => {
+    const sorted = [...(new Set(data.map(({id}) => id)))]
+    const next = Number(sorted.pop().slice(1)) + 1
+    return `${idprefix}${next}`
+}
+
+module.exports = fp(async function (fastify, opts) {
+    fastify.decorate('currentOrders', currentOrders)
+    fastify.decorate('realtimeOrders', realtimeOrders)
+    fastify.decorate('addOrder', addOrder)
+    fastify.decorateRequest('mockDataInsert', function insert (category, data) {
+        const request = this
+        const idPrefix = catToPrefix[category]
+        const id = calculateID(idPrefix, data)
+        data.push({id, ...request.body})
+    })
+})
---
+++ mock-srv/routes/orders/index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+    function monitorMessages(socket) {
+        socket.on('message', (data) => {
+            try {
+                const  { cmd, payload } = JSON.parse(data)
+                if (cmd === 'update-category') {
+                    sendCurrentOrders(payload.category, socket)
+                }
+            } catch (err) {
+                fastify.log.warn(
+                    'WebSocket Message (data: %o) Error: %s',
+                    data,
+                    err.message
+                )
+            }
+        })
+    }
+
+    function sendCurrentOrders(category, socket) {
+        for (const order of fastify.currentOrders(category)) {
+            socket.send(order)
+        }
+    }
+
+    fastify.get(
+        '/:category',
+        { websocket: true },
+        async ({ socket }, request) => {
+            monitorMessages(socket)
+            sendCurrentOrders(request.params.category, socket)
+            for await (const order of fastify.realtimeOrders()) {
+                if (socket.readyState >= socket.CLOSING) break;
+                socket.send(order)
+            }
+        }
+    )
+
+    fastify.post('/:id', async (request) => {
+        const { id } = request.params;
+        fastify.addOrder(id, request.body.amount)
+        return { ok: true }
+    })
+}
